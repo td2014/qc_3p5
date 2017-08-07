0668b92448bbc2ccc671aa74f49dd2a717cba532
td2014 <anthonyd2004@gmail.com>
2017-08-06 18:14:07 -0700

    Started QFT analysis code

---
+++ QFT_analysis.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  6 18:03:17 2017
+
+@author: anthonydaniell
+"""
+import math
+import cmath
+
+
+#
+# Fourier Transform
+# for binary represented
+# integers
+#
+L=4
+
+#
+# ENd of script
+#
